664e2156e2c9ad9426e225bd14c4a83018452f66
NitMi1234 <95387308+NitMi1234@users.noreply.github.com>
2022-03-22 18:47:50 -0400

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,12 @@
+class Game {
+  constructor() {}
+
+  //gamestate=0
+  start() {
+    //creating form object out of form class
+    form = new Form();
+    form.display();
+    //creating player class out of player class
+    player = new Player();
+  }
+}
